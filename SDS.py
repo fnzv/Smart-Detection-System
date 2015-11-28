2cd40bda24d0256ba73503164e9526de9d57cea4
Yessou Sami <yessou.sami@gmail.com>
2015-11-28 14:17:40 +0100

    Update SDS.py

--- SDS.py
+++ SDS.py
@@ -2,9 +2,10 @@
 #!/usr/bin/python
 #######  Smart Detecton System - An Easy to use Smart-Firewall coded in python
 #######  STATUS: Still under developement
-####### Features: 
+####### Features\TODO: 
 #######    - Block unwanted Sites,URLs,IP,DNS requests
-#######    - Analize processes running on host and allow only used connections      
+#######    - Analize processes running on host and allow only used connections     
+#######    - Anti-Malware Scanning realtime
 #######    - Show network connections on an easy-adaptive webGUI(Make Stats...Most used software,ip addresses,dns queries...)
 #######    - Optimize most used network connections with a local dns(file hosts)
 #######    - Parse capture files and extract ip addresses,domains,dns for firewall rules
