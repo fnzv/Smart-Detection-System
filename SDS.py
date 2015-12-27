632161b8732ff99c9a36f3af7f326588737e59df
Yessou Sami <fnzv@users.noreply.github.com>
2015-12-27 18:44:51 +0100

    Update SDS.py

--- SDS.py
+++ SDS.py
@@ -27,6 +27,10 @@ import os,time,argparse,socket,pprint
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument('-showlogs', action='store', default="none",
+                    dest='showlogs',
+                    help='Show detailed logs .. example:  live-WWW... 50-DNS')
+
 
 parser.add_argument('-banCountry', action='store', default="none",
                     dest='bancountry',
@@ -158,7 +162,21 @@ if not (results.trafflimit=="none"):
       os.popen("iptables -I INPUT -p udp -m limit --limit "+tl+" "+timeout+" -j ACCEPT")
       os.popen("iptables -I OUTPUT -p udp -m limit --limit "+tl+" "+timeout+" -j ACCEPT")
       os.popen("iptables -I FORWARD -p udp -m limit --limit "+tl+" "+timeout+" -j ACCEPT")
-
+      
+if not(results.showlogs=="none"): #  -showlogs live-WWW
+      show=results.showlogs
+      show=show.split('-')
+      if("live" in show[0]): ## 
+        ## tail -f /var/log/syslog | grep WWW | awk '{$5="  ";  $9="   ";  $10=""; $14="   "; $15=""; $17="  "; $18=""; $23="";  print $i }'
+        ## "parsed" logs to show only important info
+        rawlog=os.popen("""tail -f /var/log/syslog | grep """+show[1]+""" | awk '{$5="  ";  $9="   ";  $10=""; $14="   "; $15=""; $17="  "; $18=""; $23="";  print $i }'""").read()
+        sniff(iface= interface,filter="port 53",prn= querysniff, store= 0)
+      else:
+        #-showlogs 50-WWW   // last 50 lines of www logs + 50 dns "actual" logs (iptables syslogs but also passive dns)
+        rawlog=os.popen("""tail """+show[0]+""" /var/log/syslog | grep """+show[1]+""" | awk '{$5="  ";  $9="   ";  $10=""; $14="   "; $15=""; $17="  "; $18=""; $23="";  print $i }'""").read()
+        print "----------- DNS Queries-----------"
+        sniff(iface="eth0",filter="port 53",prn= querysniff, store= 0,count=int(show[0]))
+        
 
 if not(results.loadpcap == "none"):
                         print "Reading capture file and parsing ip addresses"
